636977dff02dbc8a470846327106b624d1a0c3d5
Barta Balázs <barta.balazs00@gmail.com>
2021-10-26 12:51:59 +0200

    Kartya osztály elkészítése

---
+++ src/hu/bartabalazs/Kartya.java
@@ -0,0 +1,20 @@
+package hu.bartabalazs;
+
+public class Kartya extends BankiSzolgaltatas{
+    private Szamla szamla;
+    private String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+    public boolean vasarlas(int osszeg){
+        boolean sikeres = szamla.kivesz(osszeg);
+        return sikeres;
+    }
+}
