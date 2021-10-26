3ed6c5b13597ffc3b8618b4d0df48043564e1774
Barta Balázs <barta.balazs00@gmail.com>
2021-10-26 10:50:11 +0200

    Bankiszolgaltatas osztály elkészítése

---
+++ src/hu/bartabalazs/BankiSzolgaltatas.java
@@ -0,0 +1,14 @@
+package hu.bartabalazs;
+
+public class BankiSzolgaltatas {
+
+    Tulajdonos tulajdonos;
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+}
